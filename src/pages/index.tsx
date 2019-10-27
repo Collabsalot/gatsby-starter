e2767fdc6ae03c80dd8d7efe4009a45da7962f73
Martin Rosenberg <Martin.B.Rosenberg@gmail.com>
2019-10-27 13:27:05 -0600

    Switch Gatsby's TypeScript plugin (#6)
    Switch from gatsby-plugin-typescript to gatsby-plugin-ts, which uses
    webpack ts-loader and typescript itself to build in lots of improvements
    over the -typescript plugin, and even bundles graphql-codegen in the
    form of gatsby-plugin-graphql-codegen.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,25 @@
+import { graphql } from 'gatsby'
 import React, { FC } from 'react'
 
-const Index: FC<{}> = () => <div>Hello world!</div>
+import { IndexQuery } from '../../@types/graphql'
+
+type IndexProps = {
+  data: IndexQuery
+}
+
+const Index: FC<IndexProps> = ({ data }) => {
+  // eslint-disable-next-line prettier/prettier
+  return <div>Hello {data?.site?.siteMetadata?.name ?? 'world'}!</div>
+}
+
+export const pageQuery = graphql`
+  query Index {
+    site {
+      siteMetadata {
+        name
+      }
+    }
+  }
+`
 
 export default Index
