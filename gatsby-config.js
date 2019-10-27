67cef18d1769ef505739dc7f8882eeac335a00c8
Martin Rosenberg <Martin.B.Rosenberg@gmail.com>
2019-10-27 13:19:31 -0600

    Switch Gatsby's TypeScript plugin
    Switch from gatsby-plugin-typescript to gatsby-plugin-ts, which uses
    webpack ts-loader and typescript itself to build in lots of improvements
    over the -typescript plugin, and even bundles graphql-codegen in the
    form of gatsby-plugin-graphql-codegen.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,22 +1,41 @@
+const siteMetadata = {
+  name: 'Steve Austin',
+}
+
+const plugins = [
+  /**
+   * @see https://www.gatsbyjs.org/packages/gatsby-plugin-ts/
+   * @see https://www.gatsbyjs.org/packages/gatsby-plugin-graphql-codegen/
+   */
+  {
+    resolve: 'gatsby-plugin-ts',
+    options: {
+      /**
+       * This cannot be in `src` or it will cause an infinite loop.
+       */
+      fileName: './@types/graphql.ts',
+
+      /**
+       * @see https://github.com/TypeStrong/fork-ts-checker-webpack-plugin#options
+       */
+      forkTsCheckerPlugin: {
+        eslint: true,
+      },
+
+      /**
+       * @see https://github.com/TypeStrong/ts-loader#options
+       */
+      tsLoader: {},
+    },
+  },
+]
+
 /**
  * @see https://www.gatsbyjs.org/docs/gatsby-config/
  */
 const config = {
-  plugins: [
-    /**
-     * @see https://www.gatsbyjs.org/packages/gatsby-plugin-typescript/
-     * @see https://babeljs.io/docs/en/babel-plugin-transform-typescript
-     * @see https://babeljs.io/docs/en/babel-preset-typescript
-     */
-    {
-      resolve: 'gatsby-plugin-typescript',
-      options: {
-        allExtensions: true,
-        allowNamespaces: true,
-        isTSX: true,
-      },
-    },
-  ],
+  plugins,
+  siteMetadata,
 }
 
 module.exports = config
