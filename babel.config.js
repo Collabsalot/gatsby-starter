e074ddf2db180071f25e1995e7899a53c3fbdfe6
Martin Rosenberg <Martin.B.Rosenberg@gmail.com>
2019-10-26 21:23:19 -0600

    Configure Babel

---
+++ babel.config.js
@@ -0,0 +1,29 @@
+/**
+ * @see https://www.gatsbyjs.org/docs/babel/
+ * @see https://babeljs.io/docs/en/configuration
+ * @see https://babeljs.io/docs/en/config-files
+ */
+const config = (api) => {
+  api.cache.using(() => process.env.NODE_ENV === 'development')
+
+  return {
+    /**
+     * @see https://babeljs.io/docs/en/babel-plugin-proposal-nullish-coalescing-operator
+     * @see https://babeljs.io/docs/en/babel-plugin-proposal-optional-chaining
+     */
+    plugins: [
+      '@babel/plugin-proposal-nullish-coalescing-operator',
+      '@babel/plugin-proposal-optional-chaining',
+    ],
+
+    /**
+     * @see https://github.com/gatsbyjs/gatsby/tree/master/packages/babel-preset-gatsby
+     * @see https://www.gatsbyjs.org/docs/browser-support/
+     */
+    presets: [
+      'babel-preset-gatsby'
+    ]
+  }
+}
+
+module.exports = config
